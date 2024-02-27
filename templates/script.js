9eedcec68c24a247a890b2daf6c4b7b12a50d28a
Gudule Lapointe <gudule@speculoos.world>
2024-02-27 02:59:50 -0400

    sticky header js code

--- templates/script.js
+++ templates/script.js
@@ -243,3 +243,14 @@ function refreshCalendar(timeZone) {
 }
 
 refreshCalendar(timeZone);
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const sticky = header.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
